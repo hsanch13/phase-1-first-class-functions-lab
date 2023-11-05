e074b2cce8aa305fed4ef5fc678bcc4f9e440dff
hsanch13 <hayleysanchez13@gmail.com>
2023-11-04 18:13:39 -0600

    made callback function

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+ 
+const allDrivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnFirstTwoDrivers = (allDrivers) => {
+    return allDrivers.slice(0, 2);   
+}
+returnFirstTwoDrivers(allDrivers)
+
+const returnLastTwoDrivers = (allDrivers) => {
+    return allDrivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(interger){
+    return function(fare){
+        return fare * interger
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(allDrivers, returnDrivers){
+    return returnDrivers(allDrivers)
+}
